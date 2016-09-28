a6e2cdcaa3add4808d2a3bb57db39a973d8ce1c8
Eero Heinänen <eero.heinanen@tut.fi>
2016-09-28 10:46:11 +0300

    Kuvia kinectistä matlabiin

---
+++ Harkkatyo.m
@@ -0,0 +1,19 @@
+%% ROS initialization, run with f9
+% Edit ROS network variables to fit your network
+%{
+setenv('ROS_MASTER_URI','http://192.168.2.90:11311')
+setenv('ROS_HOSTNAME','192.168.2.90')
+rosinit
+%}
+
+%%
+imagetopic = '/kinect2/qhd/image_color/compressed';
+imsub = rossubscriber(imagetopic, 'sensor_msgs/CompressedImage');
+
+%%
+while true
+    msg = receive(imsub, 3.5); % 0.5 sec timeout
+    image = readImage(msg);
+    imagesc(image);
+    hold on;
+end
