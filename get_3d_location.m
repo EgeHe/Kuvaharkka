b8e088b720824340e5129ff3d7b03126a65f485c
Eelis Peltola <eelispeltola@users.noreply.github.com>
2016-10-10 08:58:59 +0300

    Muunto keskiarvoksi

--- get_3d_location.m
+++ get_3d_location.m
@@ -9,16 +9,21 @@ y_end = fix(min(size(depth_image,1),coordinates(2) + roi_size/2));
 
 roi = depth_image(y_start:y_end, x_start:x_end);
 
-%Remove zeros as faulty measurements
+% % Remove zeros as faulty measurements
+%
+% % Locate center of ball (point closest to the depth camera)
+% [ball_center_z, index] = min(roi(roi~=0));
+% [y, x] = ind2sub(size(roi), index);
+% 
+% % Change to depth image coordinates
+% ball_center_x = x + x_start - 1;
+% ball_center_y = y + y_start - 1;
+% ball_center = [ball_center_x, ball_center_y, ball_center_z];
+%
 
+ball_center_z = mean(mean(roi));
 
-% Locate center of ball (point closest to the depth camera)
-[ball_center_z, index] = min(roi(roi~=0));
-[y, x] = ind2sub(size(roi), index);
-
-% Change to depth image coordinates
-ball_center_x = x + x_start - 1;
-ball_center_y = y + y_start - 1;
-ball_center = [ball_center_x, ball_center_y, ball_center_z];
+% Add depth coordinate
+ball_center = [coordinates, ball_center_z];
 
 end
